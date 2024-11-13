c9000c1a198ff473087df5852fff66971378f9ca
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-11-13 17:59:25 +0530

    React router intro

---
+++ react_router/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Footer, Header, Home } from "./components";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
