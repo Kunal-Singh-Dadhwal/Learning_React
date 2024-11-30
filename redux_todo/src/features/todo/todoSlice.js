52999520ac018da38662aaeebcd7e1a45c37af69
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-11-30 19:08:11 +0530

    Debugging

--- redux_todo/src/features/todo/todoSlice.js
+++ redux_todo/src/features/todo/todoSlice.js
@@ -26,4 +26,4 @@ export const todoSlice = createSlice({
 });
 
 export const { addTodo, removeTodo } = todoSlice.actions;
-export default todoSlice.reducer;
+export const todoReducer = todoSlice.reducer;
